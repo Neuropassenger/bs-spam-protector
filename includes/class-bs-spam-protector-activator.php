4cdc70acee0b05f9274d3354dfe0b0cef1532b98
Oleg Sokolov <turgenoid@gmail.com>
2021-06-13 23:51:37 +0300

    Init

---
+++ includes/class-bs-spam-protector-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://neuropassenger.ru
+ * @since      1.0.0
+ *
+ * @package    Bs_Spam_Protector
+ * @subpackage Bs_Spam_Protector/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Bs_Spam_Protector
+ * @subpackage Bs_Spam_Protector/includes
+ * @author     Oleg Sokolov <turgenoid@gmail.com>
+ */
+class Bs_Spam_Protector_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
